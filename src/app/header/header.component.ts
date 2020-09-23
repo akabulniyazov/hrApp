5bd22bc33841eeff43782d02b6156d6f4cda87f4
techtorial <50930535+techtorial@users.noreply.github.com>
2020-09-22 22:24:05 -0500

    Header added

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  message: string="Welcome to HR Application";
+  date: string;
+
+  constructor() {
+    setInterval( () => {
+      let currentDate = new Date();
+      this.date=currentDate.toDateString()+' '+currentDate.toLocaleTimeString();
+    } , 1000);
+   }
+
+  ngOnInit() {
+  }
+
+}
