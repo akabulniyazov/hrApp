1983b0060440df38037c684bc1593b268d1a3553
techtorial <50930535+techtorial@users.noreply.github.com>
2020-10-01 14:47:16 -0500

    asa

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/hr-app'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/hr-app/index.html'));});
+    '/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
+console.log('Console listening!')
